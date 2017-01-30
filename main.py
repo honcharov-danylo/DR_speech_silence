441de1d60a81db9ff52a7df4633223914034e2f5
mr. noone <smth@smth.com>
2017-01-30 11:42:39 +0200

    update

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if(__name__=="__main__"):
     #main("data"+os.sep+"007NTWY_U2_RC.mp3")
     #main("/home/leon/DataRoot/100_Trainee/52772751_903.wav")
     #webercvad.webercvad.parse_audio(3, path="out_test2.wav")
-    #utils.utils.get_beginning_of_file("out_test.wav","beginning_out_test.wav",60)
+    utils.utils.get_beginning_of_file("out_test.wav","beginning_out_test.wav",180)
     #utils.analysis.plot_audio_chunks_duration("temp_web")
     wb=webercvad.webercvad()
     utils.utils.save_to_file("webrtc_vad.txt",wb.send_to_google_api("beginning_out_test.wav","ru_RU",agressivity=1))
